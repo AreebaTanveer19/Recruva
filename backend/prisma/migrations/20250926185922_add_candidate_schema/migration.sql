a2185283c980a35da3fd976e86937b7b26d3b1b4
Waleeja Ali <waleeja.ali25@gmail.com>
2025-09-27 00:00:50 +0500

    fix: database

--- backend/prisma/migrations/20250926185922_add_candidate_schema/migration.sql
+++ backend/prisma/migrations/20250926185922_add_candidate_schema/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `User2` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "public"."User2";
-
 -- CreateTable
 CREATE TABLE "public"."Candidate" (
     "id" SERIAL NOT NULL,
